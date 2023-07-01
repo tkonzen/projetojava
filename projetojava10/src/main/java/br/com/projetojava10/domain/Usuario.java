6b4b7fb3e61d7d6153a7e58dbfdd2e44aa962ac6
tkonzen <tkonzen@DESKTOP-NFJIRHS.Dlink>
2023-07-01 17:00:51 -0300

    add nome

--- projetojava10/src/main/java/br/com/projetojava10/domain/Usuario.java
+++ projetojava10/src/main/java/br/com/projetojava10/domain/Usuario.java
@@ -2,4 +2,22 @@ package br.com.projetojava10.domain;
 
 public class Usuario {
 	private String sigla;
+	
+	private String nome;
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
